c2a007956a2ce4fbe55d590fe7a5c4348f63d2f3
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2019-01-07 12:52:13 +0100

    bug fix

--- client/src/actions/skiActions.js
+++ client/src/actions/skiActions.js
@@ -5,13 +5,20 @@ import {
 
 export const getskis = (data)=> dispatch=>{
     dispatch(setSkiLoading());
-    const skis = data.map(size=>
-        size.skis.map(ski=>ski)
-    )  
-    dispatch({
-        type:GET_SKIS,
-        payload:skis[0]
-    })
+    if(data.length === 0){
+        dispatch({
+            type:GET_SKIS,
+            payload:[]
+        })
+    }else{
+        const skis = data.map(size=>
+            size.skis.map(ski=>ski)
+        )  
+        dispatch({
+            type:GET_SKIS,
+            payload:skis[0]
+        })
+    }
          
 };
 
