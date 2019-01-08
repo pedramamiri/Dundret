231d120a0f2e91673a4a89dbd94b6fbba03a01a0
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2019-01-08 03:10:33 +0100

    change calculator

--- client/src/calculator/index.js
+++ client/src/calculator/index.js
@@ -15,15 +15,20 @@ function Calculator(specifi){
         if(this.classic && this.length >= 190 && this.length <= 200 ){
             this.data.skiLength = 210
             this.data.type = 'classic';
-        }else if(this.age <= 4){
+            
+        }else if(this.age <= 4 && this.length <= 130){
             this.data.skiLength = this.length
             this.classic ? this.data.type = 'classic' : this.data.type = 'freeStyle';
 
-        }else if(this.age >= 5 && this.age <= 8){
+        }else if(this.age <= 4 && this.length > 130 && this.length <=150){
             this.data.skiLength = {min:this.length + 10, max:this.length + 20}
             this.classic ? this.data.type = 'classic' : this.data.type = 'freeStyle'
 
-        }else if(this.age > 8){
+        }else if(this.age >= 5 && this.age <= 8 && this.length <= 150){
+            this.data.skiLength = {min:this.length + 10, max:this.length + 20}
+            this.classic ? this.data.type = 'classic' : this.data.type = 'freeStyle'
+
+        }else{
             if(this.classic){
                 this.data.skiLength = this.length + 20
                 this.data.type = 'classic';
